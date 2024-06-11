2c713b1122f3ec3d918c291ee633be8487e2d4d6
Dennis <dennisdao2001@hotmail.com>
2024-06-11 14:34:06 -0400

    Change Batch Script Change script to have more readable backup names.

--- backup.bat
+++ backup.bat
@@ -1,15 +1,12 @@
 echo off
 echo 'Generate backup file name'
+echo %time%
+set HH=%time:~0,2%
+set MM=%time:~3,2%
+SET SS=%time:~6,2%
+SET MS=%time:~9,2%
 
-set CUR_YYYY=%date:~10,4%
-set CUR_MM=%date:~4,2%
-set CUR_DD=%date:~7,2%
-set CUR_HH=%time:~0,2%
-if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
-set CUR_NN=%time:~3,2%
-set CUR_SS=%time:~6,2%
-
-set BACKUP_FILE=%CUR_YYYY%-%CUR_MM%-%CUR_DD%_%CUR_HH%-%CUR_NN%-%CUR_SS%.sql
+set BACKUP_FILE=%date%-%HH%%MM%%SS%%MS%.sql
 
 echo 'Backup path: %BACKUP_FILE%'
 echo 'Creating a backup ...'
